eed2c24709400ff014b2ef7f8130393007cc1f59
smmmdreza1 <s.mohammadreze.safari4@gmail.com>
2024-04-30 05:35:37 +0430

    add feature to read command line arguments and some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::collections::{BTreeMap, HashMap};
 
+use std::fs;
+use std::env::args;
+// use std::io::prelude::*;
+
+use std::error::Error;
+
+type BoxResult<T> = Result<T, Box<dyn Error>>;
+
 trait MyStr {
     fn head(&self) -> char;
     fn tail(&self) -> &Self;
@@ -8,7 +16,7 @@ trait MyStr {
 
 impl MyStr for str {
     fn head(&self) -> char {
-        return self.chars().next().unwrap();
+        return self.chars().next().expect("Err: cannot take the head of string slice beacouse the string slice is empty!");
     }
 
     fn tail(&self) -> &Self {
@@ -16,7 +24,7 @@ impl MyStr for str {
     }
 
     fn to_uint(&self) -> usize {
-        return self.parse::<usize>().unwrap();
+        return self.parse::<usize>().unwrap_or_else(|err| panic!("Err: cannont convert the string slice to an uint becouse: {}", err));
     }
 }
 
@@ -50,15 +58,38 @@ fn dizzy(data: &str) -> String {
     return ret;
 }
 
+fn read_file(path: &str) -> BoxResult<String> {
+    return Ok(fs::read_to_string(path)?);
+}
+
 fn main() {
-    let data = "T4 l16 _36 510 _27 s26 _11 320 414 {6 }39 C2 T0 m28 317 y35 d31 F1 m22 g19 d38 z34 423 l15 329 c12 ;37 19 h13 _30 F5 t7 C3 325 z33 _21 h8 n18 132 k24";
+    let args: Vec<String> = args().collect();
 
-    dbg!(undizzy(data));
+    if args.len() >= 3 {
+        let command = args[1].as_str();
 
-    let data2 = "TFCCTF{th15_ch4ll3ng3_m4k3s_m3_d1zzy_;d}";
+        if args[2] == "-f" {
+            let file_name = &args[3];
 
-    dbg!(dizzy(data2));
-    dbg!(undizzy(&dizzy(data2)));
+            let file_content = read_file(file_name).expect(&format!("Err: Cannont find or read {}!", file_name));
+
+            match command {
+                "undizzy" => println!("Result: {}", undizzy(&file_content)),
+                "dizzy"   => println!("Result: {}", dizzy(&file_content)),
+                _         => println!("You have to type a command (dizzy/undizzy)!"),
+            }
+        } else {
+            let data = &args[2..].join(" ");
+
+            match command {
+                "undizzy" => println!("Result: {}", undizzy(data)),
+                "dizzy"   => println!("Result: {}", dizzy(data)),
+                _         => println!("You have to type a command (dizzy/undizzy)!"),
+            }
+        }
+    } else {
+        println!("NOT ENOUGH ARGUMENTS!");
+    }
 }
 
 #[cfg(test)]
@@ -72,5 +103,12 @@ mod test {
         let need_to_dizzy = dizzy("TFCCTF{th15_ch4ll3ng3_m4k3s_m3_d1zzy_;d}");
 
         assert_eq!(need_to_undizzy, undizzy(&need_to_dizzy));
+    }
+
+    #[test]
+    fn is_working() {
+        // let string = "77";
+        // println!("{}", string.head());
+        // println!("{}", string.to_uint());
     }
 }
